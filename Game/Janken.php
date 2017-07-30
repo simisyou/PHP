8ba10cc534f455766111488547989c49fa7609c4
SimizuSyouta <shimizushouta@shimizushouta-no-MacBook-Pro.local>
2017-07-31 01:37:39 +0900

    じゃんけん

--- Game/Janken.php
+++ Game/Janken.php
@@ -5,7 +5,15 @@ echo '最初はグー じゃんけん!!  Gu = 1 Choki = 1 Pa = 2 ' , PHP_EOL;
 $input_result = inputhand();
 victory_or_defeat($input_result);
 
-// 勝敗判定
+// 互いの手による勝敗計算結果を取得
+function inputhand(){
+    $my_hand = (int)fscanf(STDIN , "%d");
+    $cpu_hand = rand(0,2);
+    $result = ($my_hand - $cpu_hand + 3) % 3;
+    return $result;
+}
+
+// 勝敗結果を表示
 function victory_or_defeat($input_result){
     while($input_result == '0'){
         echo 'あいこでしょ!!' , PHP_EOL;
@@ -19,12 +27,4 @@ function victory_or_defeat($input_result){
             echo '勝ち' , PHP_EOL;
             break;
    }
-}
-
-// 互いの手による勝敗計算結果を取得
-function inputhand(){
-    $my_hand = (int)fscanf(STDIN , "%d");
-    $cpu_hand = rand(0,2);
-    $result = ($my_hand - $cpu_hand + 3) % 3;
-    return $result;
 }
